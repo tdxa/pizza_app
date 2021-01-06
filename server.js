6e0615f9094d6e4d628df4422aa643e26e5d3d68
tdxa <anndoman3@gmail.com>
2021-01-06 22:42:03 +0100

    Create a homepage

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const ejs = require('ejs');
+const expressLayout = require('express-ejs-layouts');
+const path = require('path')
 
 const app = express();
 
 app.get('/', (req,res)=> {
-    res.send('Hello')
+    res.render('home')
 })
 
+app.use(expressLayout);
+app.set('views', path.join(__dirname,'/resources/views'))
+app.set('view engine','ejs')
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Example app listening on ${port} port!`));
