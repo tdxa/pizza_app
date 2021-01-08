1be339058b2a08bec0e1c8decb94845f4f1707a9
tdxa <anndoman3@gmail.com>
2021-01-08 12:49:46 +0100

    Add laravel-mix

--- server.js
+++ server.js
@@ -5,12 +5,15 @@ const path = require('path')
 
 const app = express();
 
+
+// Assets
+app.use(express.static('public'))
 app.get('/', (req,res)=> {
     res.render('home')
 })
 
 app.use(expressLayout);
-app.set('views', path.join(__dirname,'/resources/views'))
+app.set('views', path.join(__dirname,'/src/views'))
 app.set('view engine','ejs')
 
 const port = process.env.PORT || 3000;
