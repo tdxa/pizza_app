530360dbad5e41140ce0c3c8b643c5cbb691a813
tdxa <anndoman3@gmail.com>
2021-02-13 22:42:05 +0100

    Fix navigation & move routes to another file

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const expressLayout = require('express-ejs-layouts');
 const path = require('path')
 
 const app = express();
+exports.app = app;
 
 
 // Assets
@@ -13,22 +14,10 @@ app.use(expressLayout);
 app.set('views', path.join(__dirname,'/src/views'))
 app.set('view engine','ejs')
 
+// Import routes
+require('./routes/web')(app);
 
-app.get('/', (req,res)=> {
-    res.render('home')
-})
 
-app.get('/cart', (req,res)=>{
-    res.render('customers/cart')
-})
-
-app.get('/login', (req,res)=>{
-    res.render('auth/login')
-})
-
-app.get('/register', (req,res)=>{
-    res.render('auth/register')
-})
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Example app listening on ${port} port!`));
