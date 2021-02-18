593cd7581bafe6377efd3dfa8d37c44b6f4ea059
tdxa <anndoman3@gmail.com>
2021-02-19 00:06:05 +0100

    Fix creating account functionality

--- app/http/controllers/authController.js
+++ app/http/controllers/authController.js
@@ -11,50 +11,54 @@ function authController() {
             let errors = [];
 
             if (!name || !email || !password || !password2) {
-                if (password !== password2) {
-                    errors.push({ msg: "Passwords don't match" });
-                }
-                if (password.length < 6) {
-                    errors.push({ msg: 'Password is too short! Should be at least 6 characters.' })
-                }
-                if (errors.length > 0) {
-                    errors.forEach(err => {
-                        req.flash('error', err.msg)
-                    });
-                    console.log(errors)
-                    res.render('./auth/register', {
-                        errors,
-                        name,
-                        email
-                    });
-                } else {
-                    User.findOne({ email: email }).then(user => {
-                        if (user) {
-                            errors.push({ msg: 'Email is already registered!' });
-                            res.render('./auth/register', {
-                                errors,
-                                name,
-                                email
-                            });
-                        } else {
-                            const newUser = new User({
-                                name,
-                                email,
-                                password
-                            });
+                errors.push({ msg: 'Please fill in all fields!' })
+            }
 
-                            bcrypt.genSalt(10, (error, salt) =>
-                                bcrypt.hash(newUser.password, salt, (error, hash) => {
-                                    if (error) throw error;
-                                    newUser.password = hash;
+            if (password !== password2) {
+                errors.push({ msg: "Passwords don't match" });
+            }
 
-                                    newUser.save().then(user => {
-                                        res.redirect('/login');
-                                    }).catch(err => console.log(err));
-                                }));
-                        }
-                    })
-                }
+            if (password.length < 6) {
+                errors.push({ msg: 'Password is too short! Should be at least 6 characters.' })
+            }
+
+            if (errors.length > 0) {
+                errors.forEach(err => {
+                    req.flash('error', err.msg)
+                });
+                res.render('./auth/register', {
+                    errors,
+                    name,
+                    email
+                });
+            } else {
+                User.findOne({ email: email }).then(user => {
+                    if (user) {
+                        console.log(errors)
+                        req.flash('error', 'Email is already registered!')
+                        res.render('./auth/register', {
+                            errors,
+                            name,
+                        });
+
+                    } else {
+                        const newUser = new User({
+                            name,
+                            email,
+                            password
+                        });
+
+                        bcrypt.genSalt(10, (error, salt) =>
+                            bcrypt.hash(newUser.password, salt, (error, hash) => {
+                                if (error) throw error;
+                                newUser.password = hash;
+
+                                newUser.save().then(user => {
+                                    res.redirect('/login');
+                                }).catch(err => console.log(err));
+                            }));
+                    }
+                })
             }
         },
 
