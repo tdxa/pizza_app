a257dfdbfdf999118ef14e78614bcdf9b0fca1f3
tdxa <anndoman3@gmail.com>
2021-02-18 14:37:08 +0100

    Add session config

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+require('dotenv').config()
+
 const express = require('express');
 const ejs = require('ejs');
 const expressLayout = require('express-ejs-layouts');
 const path = require('path')
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongo')(session);
+
 
 const app = express();
 exports.app = app;
@@ -13,6 +18,22 @@ mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("MongoDB conncted"))
     .catch(err => console.log(err))
 
+
+
+//Session
+let MongoStore = new MongoDBStore({
+    mongooseConnection: mongoose.connection,
+    collection: 'sessions'
+})
+
+
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    store: MongoStore,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 } //24hours
+}));
 
 // Assets
 app.use(express.static('public'));
